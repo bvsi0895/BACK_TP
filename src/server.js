c3a1c4726ca062d7cffcd27cda243a9c0a6adf8e
bvsi0895 <bvsi895@gmail.com>
2023-12-03 22:09:20 -0300

    deploy back

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ server.use(express.json());
 server.use(cors());
 server.use(morgan("dev"));
 server.use((req,res, next)=>{
-    console.log(req);
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Credentials','true');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
